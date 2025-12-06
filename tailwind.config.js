97bf2f263950b4e34ef0b577c04fc65d1e3f8e7a
Shayan Akhtar Abedeen <shayanakhtarabedeen@gmail.com>
2025-12-06 11:30:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// tailwind.config.js
+// tailwind.config.js
+export default {
+  darkMode: 'class', // ← ADD THIS LINE (this enables manual dark mode)
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        blob: {
+          '0%': { transform: 'scale(1)' },
+          '33%': { transform: 'scale(1.2)' },
+          '66%': { transform: 'scale(0.8)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        blob: 'blob 10s infinite',
+      },
+      backgroundImage: {
+        'skills-gradient': 'linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%)',
+      },
+    },
+  },
+  plugins: [],
+};
